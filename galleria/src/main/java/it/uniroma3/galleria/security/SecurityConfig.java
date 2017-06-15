a00175e139c5e4f0a2528e99d8a13b54b1960e45
AlessandroCimmino <ale.cimmino@stud.uniroma3.it>
2017-06-15 20:33:37 +0200

    commit 12 - spring security

---
+++ galleria/src/main/java/it/uniroma3/galleria/security/SecurityConfig.java
@@ -0,0 +1,44 @@
+package it.uniroma3.galleria.security;
+
+import javax.sql.DataSource;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.*;
+
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+	@Autowired
+	private DataSource dataSource;
+
+
+	@Autowired
+	public void configAuthentication(AuthenticationManagerBuilder auth) throws Exception {
+		auth.jdbcAuthentication().dataSource(dataSource)
+		.usersByUsernameQuery("select username,password, enabled from users where username=?")
+		.authoritiesByUsernameQuery("select username, role from user_roles where username=?");
+	}
+
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+		http.authorizeRequests()
+		.antMatchers("/", "/home","/galleria","/cercaOpera").permitAll()
+		.anyRequest().authenticated()
+		.and()
+		.formLogin().loginPage("/login").permitAll()
+		.and()
+		.logout().permitAll();
+		
+		http.exceptionHandling().accessDeniedPage("/403");
+	}
+
+
+
+}
+
+
