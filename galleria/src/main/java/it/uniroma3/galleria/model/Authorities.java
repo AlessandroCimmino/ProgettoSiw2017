a110a0931017136e843b3fedc14d15ccd5ccc9ab
Eleonora Bartolomucci <ele.bartolomucci@stud.uniroma3.it>
2017-06-17 21:43:43 +0200

    commit 23 - login

---
+++ galleria/src/main/java/it/uniroma3/galleria/model/Authorities.java
@@ -0,0 +1,40 @@
+package it.uniroma3.galleria.model;
+
+import javax.persistence.*;
+
+@Entity
+public class Authorities {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private Long id;
+	private String username;
+	private String authority;
+	
+	public Authorities(){}
+	
+	public Authorities(String nome, String string) {
+		this.username=nome;
+		this.authority=string;
+	}
+	
+	public Long getId() {
+		return id;
+	}
+	public void setId(Long id) {
+		this.id = id;
+	}
+	public String getUsername() {
+		return username;
+	}
+	public void setUsername(String username) {
+		this.username = username;
+	}
+	public String getAuthority() {
+		return authority;
+	}
+	public void setAuthority(String authority) {
+		this.authority = authority;
+	}
+
+}
